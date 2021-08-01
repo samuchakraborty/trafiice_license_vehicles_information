6cc2e0825e016b19bf1ecdf079da6af3306b95cf
Samu Chakraborty <61682653+samuchakraborty@users.noreply.github.com>
2021-08-01 20:10:59 +0600

    remote database configure

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_Host,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
